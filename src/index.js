6fef08645516ada7f61302616218660ec240c178
Monatric <seandumlao03@gmail.com>
2025-09-21 19:53:49 +0800

    refactor: comment out unneeded code, but commented for temporary reference

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import "./styles.css";
-import { Home } from "./modules/home";
-import { Menu } from "./modules/menu";
-import { Contact } from "./modules/contact";
+// // import { Home } from "./modules/home";
+// // import { Menu } from "./modules/menu";
+// // import { Contact } from "./modules/contact";
 
-const content = document.querySelector("#content");
-const nav = document.querySelector("nav");
-let activeBtn = document.querySelector(".nav__button.active");
-let activePage = Home;
+// // const content = document.querySelector("#content");
+// // const nav = document.querySelector("nav");
+// // let activeBtn = document.querySelector(".nav__button.active");
+// // let activePage = Home;
 
-nav.addEventListener("click", (event) => {
-  if (event.target.tagName === "BUTTON") {
-    activeBtn.classList.remove("active");
-    switch (event.target.textContent) {
-      case "Home":
-        content.replaceChild(Home, activePage);
-        activePage = Home;
-        break;
-      case "Menu":
-        content.replaceChild(Menu, activePage);
-        activePage = Menu;
-        break;
-      case "Contact":
-        content.replaceChild(Contact, activePage);
-        activePage = Contact;
-        break;
-    }
+// // nav.addEventListener("click", (event) => {
+// //   if (event.target.tagName === "BUTTON") {
+// //     activeBtn.classList.remove("active");
+// //     switch (event.target.textContent) {
+// //       case "Home":
+// //         content.replaceChild(Home, activePage);
+// //         activePage = Home;
+// //         break;
+// //       case "Menu":
+// //         content.replaceChild(Menu, activePage);
+// //         activePage = Menu;
+// //         break;
+// //       case "Contact":
+// //         content.replaceChild(Contact, activePage);
+// //         activePage = Contact;
+// //         break;
+// //     }
 
-    activeBtn = event.target;
-    activeBtn.classList.add("active");
-  }
-});
+// //     activeBtn = event.target;
+// //     activeBtn.classList.add("active");
+// //   }
+// // });
 
-window.onload = function () {
-  content.appendChild(activePage);
-};
+// window.onload = function () {
+//   content.appendChild(activePage);
+// };
