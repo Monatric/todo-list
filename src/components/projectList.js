446d181a69d61fe93e8203724de57faaab0b3a00
Monatric <seandumlao03@gmail.com>
2025-10-11 19:46:48 +0800

    feat: add behavior for adding project item

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,21 +1,28 @@
+import projectItem from "./projectItem.js";
+
 const addProject = document.getElementById("add-project");
 const projectDialog = document.getElementById("project-dialog");
 const confirmBtn = document.querySelector("#project-dialog #confirm-btn");
 const cancelBtn = document.querySelector("#project-dialog #cancel-btn");
 const projectForm = document.querySelector("#project-form");
 
-addProject.addEventListener("click", () => {
-  projectDialog.showModal();
-});
+const projectList = () => {
+  addProject.addEventListener("click", () => {
+    projectDialog.showModal();
+  });
 
-projectForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log("submitted");
-  projectDialog.close();
-});
+  projectForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newProjectItem = projectItem.create(formData.get("project-name"));
+    const navList = document.querySelector(".project__list .nav__list");
+    navList.append(newProjectItem);
+    projectDialog.close();
+  });
 
-cancelBtn.addEventListener("click", () => {
-  projectDialog.close();
-});
+  cancelBtn.addEventListener("click", () => {
+    projectDialog.close();
+  });
+};
 
-export default addProject;
+export default projectList;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
 import "./styles.css";
-import addProject from "./components/projectList";
+import projectList from "./components/projectList.js";
 
-// // import { Home } from "./modules/home";
-// // import { Menu } from "./modules/menu";
-// // import { Contact } from "./modules/contact";
-
-// // const content = document.querySelector("#content");
-// // const nav = document.querySelector("nav");
-// // let activeBtn = document.querySelector(".nav__button.active");
-// // let activePage = Home;
-
-// // nav.addEventListener("click", (event) => {
-// //   if (event.target.tagName === "BUTTON") {
-// //     activeBtn.classList.remove("active");
-// //     switch (event.target.textContent) {
-// //       case "Home":
-// //         content.replaceChild(Home, activePage);
-// //         activePage = Home;
-// //         break;
-// //       case "Menu":
-// //         content.replaceChild(Menu, activePage);
-// //         activePage = Menu;
-// //         break;
-// //       case "Contact":
-// //         content.replaceChild(Contact, activePage);
-// //         activePage = Contact;
-// //         break;
-// //     }
-
-// //     activeBtn = event.target;
-// //     activeBtn.classList.add("active");
-// //   }
-// // });
-
-// window.onload = function () {
-//   content.appendChild(activePage);
-// };
+// start behavior of projectList
+projectList();
