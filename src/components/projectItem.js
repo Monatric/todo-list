9aadcf4c9b401bbba0ab5e9c2c3d0aff4cfc2f1f
Monatric <seandumlao03@gmail.com>
2025-10-11 20:54:19 +0800

    fix: scoping issues and not making new list

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -2,22 +2,23 @@ import hashIcon from "../icons/hash.svg";
 import dotMenuIcon from "../icons/dot-menu.svg";
 
 const navItemFragment = document.createDocumentFragment();
-const listItem = document.createElement("li");
-listItem.classList.add("nav__item", "item");
-
-const hashIconImg = document.createElement("img");
-hashIconImg.src = hashIcon;
-hashIconImg.alt = "Hash svg";
-hashIconImg.classList.add("svg");
-
-const dotMenuIconImg = document.createElement("img");
-dotMenuIconImg.src = dotMenuIcon;
-dotMenuIconImg.alt = "Dot menu";
-dotMenuIconImg.classList.add("svg");
 
 const projectItem = (function () {
   const create = (projectItemName) => {
-    const para = document.createElement("para");
+    const listItem = document.createElement("li");
+    listItem.classList.add("nav__item", "item");
+
+    const hashIconImg = document.createElement("img");
+    hashIconImg.src = hashIcon;
+    hashIconImg.alt = "Hash svg";
+    hashIconImg.classList.add("svg");
+
+    const dotMenuIconImg = document.createElement("img");
+    dotMenuIconImg.src = dotMenuIcon;
+    dotMenuIconImg.alt = "Dot menu";
+    dotMenuIconImg.classList.add("svg");
+
+    const para = document.createElement("p");
     para.textContent = projectItemName;
 
     listItem.append(hashIconImg, para, dotMenuIconImg);
