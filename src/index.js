3bd369e85a2ac254ecdb5d1a0f6d22e1d4625497
Monatric <seandumlao03@gmail.com>
2025-10-11 20:04:28 +0800

    feat: add localStorage checker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import "./styles.css";
 import projectList from "./components/projectList.js";
+import storageAvailable from "./components/localStorageChecker.js";
+
+// Check for localStorage
+if (storageAvailable("localStorage")) {
+  console.log("Keep using localstorage");
+} else {
+  console.log("No more localstorage");
+}
 
 // start behavior of projectList
 projectList();
